d8eea3e0f7aa24ba7f14bfaf7fe3540dd416f92e
Yahiewi <yahiahenien@gmail.com>
2026-05-20 14:19:52 +0200

    Fix test config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  snapshotDir: '.',
   fullyParallel: false,
   workers: 1,
   forbidOnly: !!process.env.CI,
